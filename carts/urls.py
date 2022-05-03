698e2308a9eeeae3e60763a8393b75c61d2f9659
Bibek Chaudhary <kingbibekc@gmail.com>
2022-05-03 14:18:12 +0545

    add to cart and remove item from cart with the variations of product

--- carts/urls.py
+++ carts/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.showCart, name='show-cart'),
     path('add-cart/<int:product_id>/', views.addCart, name='add-cart'),
-    path('remove-cart/<int:product_id>/',
+    path('remove-cart/<int:product_id>/<int:cart_item_id>/',
          views.remove_cart, name='remove-cart'),
-    path('remove-cart-item/<int:product_id>/',
+    path('remove-cart-item/<int:product_id>/<int:cart_item_id>/',
          views.remove_cart_item, name='remove-cart-item'),
 
 ]
